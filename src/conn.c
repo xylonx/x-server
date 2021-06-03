6db06e14f4ff47ae18c31472c669a2723f2f30d9
xylonx <x956200361@gmail.com>
2021-06-04 00:09:13 +0800

    add epoll connection handle module

--- src/conn.c
+++ src/conn.c
@@ -1,15 +1,51 @@
+#define _GNU_SOURCE
 #include "conn.h"
 
 #include <arpa/inet.h>
+#include <asm-generic/errno-base.h>
+#include <errno.h>
+#include <limits.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
+#include <sys/epoll.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 #include "conf.h"
+// #include "ctrl.h"
 
-int start_listen() {
+void epoll_wait_error_handle(int errsv) {
+    switch (errsv) {
+        case EBADF:
+            printf("[Error]: epoll_wait: the epfd is not valid\n");
+            return;
+
+        case EFAULT:
+            printf(
+                "[Error] epoll_wait: the events memory is not "
+                "writeable.\n");
+            return;
+
+        case EINVAL:
+            printf(
+                "[Error]: epoll_wait: epfd is not an epoll file "
+                "descriptor, or maxevents is less than or equal to "
+                "zero.");
+            return;
+    }
+}
+
+void get_cli_info(const struct sockaddr* sockaddr, char* buf, int buf_size) {
+    struct sockaddr_in* cli_addr_in = (struct sockaddr_in*)sockaddr;
+    char ipv4buf[16];
+    inet_ntop(AF_INET, &cli_addr_in->sin_addr, ipv4buf, 16);
+    snprintf(ipv4buf, sizeof(ipv4buf), "%s:%d", ipv4buf,
+             ntohs(cli_addr_in->sin_port));
+}
+
+int start_listen(char* addr, int port) {
     int listenfd;
     struct sockaddr_in servaddr;
 
@@ -18,10 +54,10 @@ int start_listen() {
     // init and set servaddr
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    inet_pton(AF_INET, conf->server.bind_addr, &servaddr.sin_addr.s_addr);
-    servaddr.sin_port = htons(conf->server.bind_port);
+    inet_pton(AF_INET, addr, &servaddr.sin_addr);
+    servaddr.sin_port = htons(port);
 
-    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+    if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
         // TODO: log
         printf("[Error] Cannot bind address %s at port %d\n",
                conf->server.bind_addr, conf->server.bind_port);
@@ -36,4 +72,163 @@ int start_listen() {
     }
 
     return listenfd;
+}
+
+// handle connection with epoll module.
+// in_handle is used to handle received tcp message. Here is http stream.
+// err_handle is used to handle abnormal connection close event
+void* handle_conn_epoll(int listenfd, req_handle in_handle,
+                        req_handle err_handle) {
+    int epollfd, awake_event_num, connfd;
+    struct epoll_event init_ev, awake_ev, conn_ev, events[MAX_EVNET];
+    char cli_info_buf[CLI_INFO_BUF_SIZE];
+
+    // client info holder
+    struct sockaddr cli_addr[MAX_FD];
+    socklen_t cli_socklen[MAX_FD];
+
+    // create epoll instance
+    if ((epollfd = epoll_create1(0)) < 0) {
+        // TODO: log
+        printf("[Error] create epoll file descriptor failure.\n");
+        exit(EXIT_FAILURE);
+        return NULL;
+    }
+
+    // init first epoll event: tcp socket listen
+    init_ev.events = EPOLLIN;
+    init_ev.data.fd = listenfd;
+
+    // add tcp socket listen event
+    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listenfd, &init_ev) == -1) {
+        // TODO: log
+        printf("[Error]: epoll_ctl: add listen file descriptor failure.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // handle events
+    for (;;) {
+        if ((awake_event_num = epoll_wait(epollfd, events, MAX_EVNET,
+                                          EPOLL_WAIL_DURATION)) < 0) {
+            // TODO: log
+            int errsv = errno;
+            epoll_wait_error_handle(errsv);
+            exit(EXIT_FAILURE);
+            return NULL;
+        }
+
+        for (int i = 0; i < awake_event_num; i++) {
+            awake_ev = events[i];
+
+            // handle new connection establish event
+            if (awake_ev.data.fd == listenfd) {
+                // init client info holder
+                bzero(&cli_addr[awake_ev.data.fd], sizeof(struct sockaddr));
+                bzero(&cli_socklen[awake_ev.data.fd], sizeof(socklen_t));
+                bzero(cli_info_buf, CLI_INFO_BUF_SIZE);
+                get_cli_info(&cli_addr[awake_ev.data.fd], cli_info_buf,
+                             CLI_INFO_BUF_SIZE);
+
+                if ((connfd = accept4(listenfd, &cli_addr[awake_ev.data.fd],
+                                      &cli_socklen[awake_ev.data.fd],
+                                      SOCK_NONBLOCK)) == -1) {
+                    // TODO: log
+                    printf(
+                        "[Warning] accept connection failure. remote client "
+                        "info: %s\n",
+                        cli_info_buf);
+
+                    // accept error, should not continue running. Interrupt this
+                    // accept.
+                    continue;
+                };
+
+                printf("[Info] client info: %s\n", cli_info_buf);
+
+                bzero(&conn_ev, sizeof(conn_ev));
+
+// set epoll trigger type
+#ifdef EPOLL_CONN_ET
+                conn_ev.events = EPOLLIN | EPOLLRDHUP | EPOLLET;
+#else
+                conn_ev.events = EPOLLIN | EPOLLRDHUP;
+#endif
+                conn_ev.data.fd = connfd;
+
+                // add connection to epoll event list
+                if (epoll_ctl(epollfd, EPOLL_CTL_ADD, connfd, &conn_ev) == -1) {
+                    // TODO: log
+                    printf("[Warning] add connection to epoll failed\n");
+                    continue;
+                }
+            }
+
+            // handle tcp connection
+            else {
+                // handle abnormal closed connection
+                if ((awake_ev.events & EPOLLRDHUP) == EPOLLRDHUP) {
+                    /*** user defined abnormal connection close handle ***/
+                    if (err_handle) {
+                        err_handle(awake_ev.data.fd,
+                                   &cli_addr[awake_ev.data.fd],
+                                   &cli_socklen[awake_ev.data.fd]);
+                    }
+
+                    // delete it from epoll list
+                    if (epoll_ctl(epollfd, EPOLL_CTL_DEL, awake_ev.data.fd,
+                                  &awake_ev) == -1) {
+                        // TODO: log
+                        printf(
+                            "[Warning] epoll delete abnormal closed connection "
+                            "failed\n");
+                        continue;
+                    }
+                    printf("[Info] close connection with fd %d\n",
+                           awake_ev.data.fd);
+                    close(events[i].data.fd);
+
+                    continue;
+                }
+
+                // handle normal connection
+                if ((awake_ev.events & EPOLLIN) == EPOLLIN) {
+                    if (!in_handle) {
+                        continue;
+                    }
+
+                    /*** user defined tcp stream handle ***/
+                    serv_verb_t* verb = (serv_verb_t*)in_handle(
+                        awake_ev.data.fd, &cli_addr[awake_ev.data.fd],
+                        &cli_socklen[awake_ev.data.fd]);
+
+                    if (verb == NULL) {
+                        continue;
+                    }
+
+                    switch (*verb) {
+                        // TODO: rebuild below codes. it duplicates.
+                        case VERB_CLOSE_CONN:
+                            if (epoll_ctl(epollfd, EPOLL_CTL_DEL,
+                                          awake_ev.data.fd, &awake_ev) == -1) {
+                                // TODO: log
+                                printf(
+                                    "[Warning] epoll delete abnormal closed "
+                                    "connection "
+                                    "failed\n");
+                                continue;
+                            }
+                            printf("[Info] close connection with fd %d\n",
+                                   awake_ev.data.fd);
+                            close(events[i].data.fd);
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    return NULL;
 }
--- src/conn.h
+++ src/conn.h
@@ -1,9 +1,32 @@
 #ifndef X_SERVER_CONN_H
 #define X_SERVER_CONN_H
 
-#define LISTEN_Q 12
+#include <sys/socket.h>
+
+#define LISTEN_Q 5
+
+#define CLI_INFO_BUF_SIZE 22
+
+// epoll related
+#define MAX_FD 1024
+#define MAX_EVNET 256
+#define EPOLL_WAIL_DURATION 10
+#define EPOLL_CONN_ET
+
+// http related
+#define VERB_CLOSE_CONN 1
+
+// the value of it is the server action.
+// e.g. close connection.
+typedef int serv_verb_t;
+
+// fd is the connection file descriptor
+// client_addr and cli_addrlen contain client info
+typedef void* (*req_handle)(int, struct sockaddr*, socklen_t*);
 
 // return listen file descriptor
-int start_listen();
+int start_listen(char* addr, int port);
+void* handle_conn_epoll(int listenfd, req_handle in_handle,
+                        req_handle err_handle);
 
 #endif

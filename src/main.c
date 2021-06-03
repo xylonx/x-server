4c1e8c04d21f51d8fb6d856d25780e0853f71e5b
xylonx <x956200361@gmail.com>
2021-06-04 00:10:03 +0800

    add simple tcp echo test func (all module successfully now)

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "conf.h"
 #include "conn.h"
@@ -6,14 +8,26 @@
 
 conf_t conf;
 
+void* echo_handle(int fd, struct sockaddr* cli_addr, socklen_t* cli_addrlen);
+
 int main(int argc, char const* argv[]) {
     init_conf(&conf);
 
-    int listenfd = start_listen();
+    int listenfd = start_listen(conf->server.bind_addr, conf->server.bind_port);
 
-    char buffer[65535] = {0};
-    // for test
-    msg_ok(12, buffer, 65535, NULL, NULL);
+    // for test. tcp echo server
+    handle_conn_epoll(listenfd, echo_handle, NULL);
 
     return 0;
+}
+
+#define BUFFER_SIZE 1024
+void* echo_handle(int connfd, struct sockaddr* addr, socklen_t* addrlen) {
+    printf("[Test] receive file descriptor: %d\n", connfd);
+    char buffer[BUFFER_SIZE];
+    bzero(buffer, BUFFER_SIZE);
+    read(connfd, buffer, BUFFER_SIZE);
+    printf("read bytes from %d:\n%s\n", connfd, buffer);
+    write(connfd, buffer, strlen(buffer));
+    return NULL;
 }

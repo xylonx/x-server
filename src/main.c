2071c304d8fd1aa33f38b9bf05b5d6bd48116074
xylonx <x956200361@gmail.com>
2021-06-04 22:52:29 +0800

    add req handle in main to run

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 
 #include "conf.h"
 #include "conn.h"
+#include "req.h"
 #include "resp.h"
 
 conf_t conf;
@@ -12,22 +13,24 @@ void* echo_handle(int fd, struct sockaddr* cli_addr, socklen_t* cli_addrlen);
 
 int main(int argc, char const* argv[]) {
     init_conf(&conf);
+    init_router_handle();
 
     int listenfd = start_listen(conf->server.bind_addr, conf->server.bind_port);
 
+    handle_conn_epoll(listenfd, http_req_handle, NULL);
     // for test. tcp echo server
-    handle_conn_epoll(listenfd, echo_handle, NULL);
+    // handle_conn_epoll(listenfd, echo_handle, NULL);
 
     return 0;
 }
 
-#define BUFFER_SIZE 1024
-void* echo_handle(int connfd, struct sockaddr* addr, socklen_t* addrlen) {
-    printf("[Test] receive file descriptor: %d\n", connfd);
-    char buffer[BUFFER_SIZE];
-    bzero(buffer, BUFFER_SIZE);
-    read(connfd, buffer, BUFFER_SIZE);
-    printf("read bytes from %d:\n%s\n", connfd, buffer);
-    write(connfd, buffer, strlen(buffer));
-    return NULL;
-}
+// #define BUFFER_SIZE 1024
+// void* echo_handle(int connfd, struct sockaddr* addr, socklen_t* addrlen) {
+//     printf("[Test] receive file descriptor: %d\n", connfd);
+//     char buffer[BUFFER_SIZE];
+//     bzero(buffer, BUFFER_SIZE);
+//     read(connfd, buffer, BUFFER_SIZE);
+//     printf("read bytes from %d:\n%s\n", connfd, buffer);
+//     write(connfd, buffer, strlen(buffer));
+//     return NULL;
+// }

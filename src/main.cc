904f44b2d14ebf8c7f1059a668ae3dff6b43ce32
xylonx <xiangyuxin.xylonx@bytedance.com>
2022-08-05 18:28:47 +0800

    :sparkles: (main) impl project main func with interface

--- src/main.cc
+++ src/main.cc
@@ -15,38 +15,31 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <string>
+
+#include "argparse.h"
+#include "except.h"
+#include "globals.h"
+#include "http/server.h"
 #include "spdlog/spdlog.h"
 
 int main(int argc, char const* argv[]) {
-    spdlog::info("Welcome to spdlog!");
-    spdlog::error("Some error message with arg: {}", 1);
+    char addr[16]   = {0};
+    char root[4096] = {0};
+    char index[16]  = {0};
+    int port        = 0;
 
-    spdlog::warn("Easy padding in numbers like {:08d}", 12);
-    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-    spdlog::info("Support for floats {:03.2f}", 1.23456);
-    spdlog::info("Positional args are {1} {0}..", "too", "supported");
-    spdlog::info("{:<30}", "left aligned");
+    xserver::OptParse parser;
+    parser.AddOption("addr", addr, 16, "0.0.0.0");
+    parser.AddOption("port", &port, 7788);
+    parser.AddOption("root", root, 4096, ".");
+    parser.AddOption("index", index, 16, "");
+    parser.Parse(argc, argv);
 
-    spdlog::set_level(spdlog::level::debug);  // Set global log level to debug
-    spdlog::debug("This message should be displayed..");
-
-    // change log pattern
-    spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
-
-    // Compile time log levels
-    // define SPDLOG_ACTIVE_LEVEL to desired level
-    SPDLOG_TRACE("Some trace message with param {}", 42);
-    SPDLOG_DEBUG("Some debug message");
-    return 0;
+    xserver::HTTPServer server;
+    try {
+        server.Serve(addr, port, root, index);
+    } catch (const xserver::XServerExcept& e) {
+        spdlog::error("http server serve failed: {}", e.what());
+    }
 }
-
-// #define BUFFER_SIZE 1024
-// void* echo_handle(int connfd, struct sockaddr* addr, socklen_t* addrlen) {
-//     printf("[Test] receive file descriptor: %d\n", connfd);
-//     char buffer[BUFFER_SIZE];
-//     bzero(buffer, BUFFER_SIZE);
-//     read(connfd, buffer, BUFFER_SIZE);
-//     printf("read bytes from %d:\n%s\n", connfd, buffer);
-//     write(connfd, buffer, strlen(buffer));
-//     return NULL;
-// }

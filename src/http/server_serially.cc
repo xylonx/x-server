3a8e858db76661479c63155eb85f15627abd18d3
xylonx <x956200361@gmail.com>
2022-08-08 01:23:38 +0800

    :sparkles: (server) impl serially server interface

---
+++ src/http/server_serially.cc
@@ -0,0 +1,58 @@
+/*
+ * This file is part of the xylonx distribution (https://github.com/xylonx).
+ * Copyright (c) 2022 xylonx.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <sys/errno.h>
+
+#include "const.h"
+#include "except.h"
+#include "globals.h"
+#include "http/request.h"
+#include "http/server.h"
+#include "spdlog/spdlog.h"
+
+namespace xserver {
+
+HTTPServer::HTTPServer() {
+    cli_addr_    = new (struct sockaddr_in);
+    cli_socklen_ = new socklen_t;
+}
+
+void HTTPServer::Serve(const char *addr, int port, const char *root, const char *index) {
+    int listen_fd = ListenAndBind(addr, port);
+
+    spdlog::info("xserver start listen at {}:{}", addr, port);
+
+    while (true) {
+        int connfd = accept(listen_fd, reinterpret_cast<struct sockaddr *>(cli_addr_), cli_socklen_);
+        if (connfd < 0) {
+            spdlog::error("failed accept conn {}: {}", connfd, std::strerror(errno));
+            continue;
+        }
+        spdlog::info("accept new connection {}", connfd);
+
+        requests_[connfd] = new Request(connfd, kMAX_REQUEST_LINE_LEN, kMAX_HEADER_LEN, read_buffer_size,
+                                        write_buffer_size, root, index);
+        try {
+            HandleConnIn(connfd);
+        } catch (const XServerExcept &e) {
+            spdlog::error("handler failed. connection {} will be closed. {}", connfd, e.what());
+            delete requests_[connfd];
+        }
+    }
+}
+
+}  // namespace xserver

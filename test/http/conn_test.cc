6d85fb58b5b8617621f2caa5b612b45a1423aff9
xylonx <x956200361@gmail.com>
2022-08-08 01:14:16 +0800

    :construction: (conn) add conn test codes

---
+++ test/http/conn_test.cc
@@ -0,0 +1,30 @@
+/*
+ * This file is part of the xylonx distribution (https://github.com/xylonx).
+ * Copyright (c) 2022 xylonx.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "http/conn.h"
+
+#include "gtest/gtest.h"
+#include "spdlog/spdlog.h"
+
+// TODO(xylonx): to be impl
+TEST(ConnTinyReadTest, BasicAssertions) {}
+
+TEST(ConnLargeReadTest, BasicAssertions) {}
+
+TEST(ConnTinyWriteTest, BasicAssertions) {}
+
+TEST(ConnLargeCloseTest, BasicAssertions) {}
